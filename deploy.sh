ea003b83d445818c251ee030a303fc0193b64263
Ilya Davydov <razzor58@gmail.com>
2019-01-06 13:59:59 +0000

    HM4: fix .sh to complete test1

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Получение кода, установка зависимостей через bundler и запуск приложения.
 git clone -b monolith https://github.com/express42/reddit.git
 cd reddit && bundle install
 puma -d
+
--- install_ruby.sh
+++ install_ruby.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-#  установка ruby
 sudo apt update
 sudo apt install -y ruby-full ruby-bundler build-essential
+
