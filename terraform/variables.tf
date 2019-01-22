e124a8bbd532edac62b6ff949b661f34797c0d2c
Ilya Davydov <razzor58@gmail.com>
2019-01-22 15:29:00 +0000

    terraform 2

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -4,16 +4,21 @@ variable project {
 
 variable region {
   description = "Region for Provider"
-  default     = "europe-west1"
+  default = "europe-west1"
+}
+
+variable zone {
+  description = "Region for app Image"
+  default = "europe-west1"
 }
 
 variable vm_region {
   description = "Region fo VM"
-  default     = "europe-west1"
+  default = "europe-west1"
 }
 
 variable public_key_path {
-  description = "user public key for GCP"
+  description = "Path to the public key used to connect to instance"
 }
 
 variable private_key_path {
@@ -22,4 +27,14 @@ variable private_key_path {
 
 variable disk_image {
   description = "Disk image"
+}
+
+variable app_disk_image {
+  description = "Disk image for reddit app"
+  default = "reddit-app-base"
+}
+
+variable db_disk_image {
+  description = "Disk image for reddit db"
+  default = "reddit-db-base"
 }
