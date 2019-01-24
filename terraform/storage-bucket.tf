3675a603e5264e5ac94699f79290f1310ffc02d7
Ilya Davydov <razzor58@gmail.com>
2019-01-24 15:21:24 +0000

    t2

---
+++ terraform/storage-bucket.tf
@@ -0,0 +1,16 @@
+provider "google" {
+  version = "1.4.0"
+  project = "${var.project}"
+  region = "${var.region}"
+}
+module "storage-bucket" {
+  source = "SweetOps/storage-bucket/google"
+  version = "0.1.1"
+  name = [
+    "sb-razzor58-stage",
+    "sb-razzor58-prod"]
+}
+output storage-bucket_url {
+  value = "${module.storage-bucket.url}"
+}
+
